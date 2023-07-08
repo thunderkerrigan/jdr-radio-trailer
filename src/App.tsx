7b8a016dee4df5b45b6b964dee7e0c7c4097262b
Joseph Pasqualini <thunderkerrrigan@gmail.com>
2023-07-08 15:56:47 +0200

    fix warmings

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
-import parisSong1 from "./assets/Alerte_Bombardement_Paris_1940.mp3";
-import parisSong2 from "./assets/Lili_Marlene.mp3";
-import parisSong3 from "./assets/Maurice_Chevalier_Fleur_De_Paris.mp3";
-import parisSong4 from "./assets/Radio-Paris.mp3";
+// import parisSong1 from "./assets/Alerte_Bombardement_Paris_1940.mp3";
+// import parisSong2 from "./assets/Lili_Marlene.mp3";
+// import parisSong3 from "./assets/Maurice_Chevalier_Fleur_De_Paris.mp3";
+// import parisSong4 from "./assets/Radio-Paris.mp3";
 
-import londonSong1 from "./assets/LE_CHANT_DES_PARTISANS.mp3";
-import londonSong2 from "./assets/radio_londres.mp3";
-import londonSong3 from "./assets/Well_Meet_Again.mp3";
-import londonSong4 from "./assets/Radio_Paris_ment_Messages_perso.mp3";
+// import londonSong1 from "./assets/LE_CHANT_DES_PARTISANS.mp3";
+// import londonSong2 from "./assets/radio_londres.mp3";
+// import londonSong3 from "./assets/Well_Meet_Again.mp3";
+// import londonSong4 from "./assets/Radio_Paris_ment_Messages_perso.mp3";
 
-import germanSong1 from "./assets/german_radio.mp3";
-import germanSong2 from "./assets/morse.mp3";
+// import germanSong1 from "./assets/german_radio.mp3";
+// import germanSong2 from "./assets/morse.mp3";
 
 import tuningSound from "./assets/Radio_Tuning.mp3";
 import switchSound from "./assets/radio_click_button.wav";
 
 import Radio from "./Radio/Radio";
-import useAudioPlaylist from "./hooks/useRadio";
+// import useAudioPlaylist from "./hooks/useRadio";
 import Backdrop from "./Backdrop/Backdrop";
 import useAudioURL from "./hooks/useTrueRadio";
 
-const parisPlaylist = [parisSong1, parisSong2, parisSong3, parisSong4];
-const londonPlaylist = [londonSong1, londonSong2, londonSong3, londonSong4];
-const germanPlaylist = [germanSong1, germanSong2];
+// const parisPlaylist = [parisSong1, parisSong2, parisSong3, parisSong4];
+// const londonPlaylist = [londonSong1, londonSong2, londonSong3, londonSong4];
+// const germanPlaylist = [germanSong1, germanSong2];
 
 function App() {
   const [hasStartRadio, startRadio] = useState<boolean>(false);
